c938bffdf3649f2bee46a595471dd95902fe14d8
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2023-09-26 15:30:59 +0300

    feat(fastlog): add ability to change item separator and line ending

--- fastlog.go
+++ fastlog.go
@@ -5,37 +5,82 @@ import (
 	"time"
 )
 
-var (
-	stdoutFile        *os.File = os.Stdout
-	stderrFile        *os.File = os.Stderr
-	IsEnableDebugLogs bool     = true
-	InfoTitle         string   = " INF "
-	DebugTitle        string   = " DBG "
-	ErrorTitle        string   = " ERR "
-	FatalTitle        string   = " FTL "
-	TimeFormat        string   = "2006-01-02T15:04:05-07:00"
-)
-
 const (
+	WHITESPACE string = " "
+	TAB        string = "\t"
 	NEW_LINE   string = "\n"
 	ERROR_WORD string = ". error: "
 )
 
+var (
+	stdoutFile        *os.File = os.Stdout
+	stderrFile        *os.File = os.Stderr
+	IsEnableDebugLogs bool     = true
+	ItemSeparator     string   = WHITESPACE
+	LineEnding        string   = NEW_LINE
+	InfoItem          string   = "INF"
+	DebugItem         string   = "DBG"
+	ErrorItem         string   = "ERR"
+	FatalItem         string   = "FTL"
+	TimeFormat        string   = "2006-01-02T15:04:05-07:00"
+)
+
 func Info(msg string) {
-	stdoutFile.Write([]byte(time.Now().Format(TimeFormat) + InfoTitle + msg + NEW_LINE))
+	var (
+		endMsg string = time.Now().Format(TimeFormat) +
+			ItemSeparator +
+			InfoItem +
+			ItemSeparator +
+			msg +
+			LineEnding
+	)
+
+	stdoutFile.Write([]byte(endMsg))
 }
 
 func Debug(msg string) {
+	var (
+		endMsg string = time.Now().Format(TimeFormat) +
+			ItemSeparator +
+			DebugItem +
+			ItemSeparator +
+			msg +
+			LineEnding
+	)
+
 	if IsEnableDebugLogs {
-		stdoutFile.Write([]byte(time.Now().Format(TimeFormat) + DebugTitle + msg + NEW_LINE))
+		stdoutFile.Write([]byte(endMsg))
 	}
 }
 
 func Error(desc string, err error) {
-	stderrFile.Write([]byte(time.Now().Format(TimeFormat) + ErrorTitle + desc + ERROR_WORD + err.Error() + NEW_LINE))
+	var (
+		endMsg string = time.Now().Format(TimeFormat) +
+			ItemSeparator +
+			ErrorItem +
+			ItemSeparator +
+			desc +
+			ERROR_WORD +
+			err.Error() +
+			LineEnding
+	)
+
+	stderrFile.Write([]byte(endMsg))
 }
 
 func Fatal(desc string, err error) {
-	stderrFile.Write([]byte(time.Now().Format(TimeFormat) + FatalTitle + desc + ERROR_WORD + err.Error() + NEW_LINE))
+	var (
+		endMsg string = time.Now().Format(TimeFormat) +
+			ItemSeparator +
+			FatalItem +
+			ItemSeparator +
+			desc +
+			ERROR_WORD +
+			err.Error() +
+			LineEnding
+	)
+
+	stderrFile.Write([]byte(endMsg))
+
 	os.Exit(1)
 }
