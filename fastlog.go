8e1287c3125d3ba3acfa80ab4afa3a9e2ca00788
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2023-09-26 17:26:17 +0300

    refactor(fastlog): reorganize constants, do structure storing

--- fastlog.go
+++ fastlog.go
@@ -5,82 +5,104 @@ import (
 	"time"
 )
 
+type logMessage struct {
+	datetime    *string
+	messageType *string
+	message     *string
+}
+
 const (
-	WHITESPACE string = " "
-	TAB        string = "\t"
-	NEW_LINE   string = "\n"
-	ERROR_WORD string = ". error: "
+	WHITESPACE         string = " "
+	DOT                string = "."
+	COMMA              string = ","
+	SEMICOLON          string = ";"
+	COLON              string = ":"
+	TAB                string = "\t"
+	NEW_LINE           string = "\n"
+	ERROR_WORD         string = ". error: "
+	INFO_MESSAGE_TYPE  string = "INF"
+	DEBUG_MESSAGE_TYPE string = "DBG"
+	ERROR_MESSAGE_TYPE string = "ERR"
+	FATAL_MESSAGE_TYPE string = "FTL"
+	TIME_FORMAT        string = "2006-01-02T15:04:05-07:00"
 )
 
 var (
-	stdoutFile        *os.File = os.Stdout
-	stderrFile        *os.File = os.Stderr
-	IsEnableDebugLogs bool     = true
-	ItemSeparator     string   = WHITESPACE
-	LineEnding        string   = NEW_LINE
-	InfoItem          string   = "INF"
-	DebugItem         string   = "DBG"
-	ErrorItem         string   = "ERR"
-	FatalItem         string   = "FTL"
-	TimeFormat        string   = "2006-01-02T15:04:05-07:00"
+	stdoutFile        *os.File    = os.Stdout
+	stderrFile        *os.File    = os.Stderr
+	IsEnableDebugLogs bool        = true
+	ItemSeparator     string      = WHITESPACE
+	LineEnding        string      = NEW_LINE
+	InfoMessageType   string      = INFO_MESSAGE_TYPE
+	DebugMessageType  string      = DEBUG_MESSAGE_TYPE
+	ErrorMessageType  string      = ERROR_MESSAGE_TYPE
+	FatalMessageType  string      = FATAL_MESSAGE_TYPE
+	TimeFormat        string      = TIME_FORMAT
+	logMsg            *logMessage = &logMessage{}
 )
 
 func Info(msg string) {
-	var (
-		endMsg string = time.Now().Format(TimeFormat) +
-			ItemSeparator +
-			InfoItem +
-			ItemSeparator +
-			msg +
-			LineEnding
-	)
+	*logMsg.datetime = time.Now().Format(TimeFormat)
+	*logMsg.messageType = InfoMessageType
+	*logMsg.message = msg
 
-	stdoutFile.Write([]byte(endMsg))
+	stdoutFile.Write(
+		[]byte(*logMsg.datetime +
+			ItemSeparator +
+			*logMsg.messageType +
+			ItemSeparator +
+			*logMsg.message +
+			LineEnding,
+		),
+	)
 }
 
 func Debug(msg string) {
-	var (
-		endMsg string = time.Now().Format(TimeFormat) +
-			ItemSeparator +
-			DebugItem +
-			ItemSeparator +
-			msg +
-			LineEnding
-	)
+	*logMsg.datetime = time.Now().Format(TimeFormat)
+	*logMsg.messageType = DebugMessageType
+	*logMsg.message = msg
 
-	if IsEnableDebugLogs {
-		stdoutFile.Write([]byte(endMsg))
-	}
+	stdoutFile.Write(
+		[]byte(*logMsg.datetime +
+			ItemSeparator +
+			*logMsg.messageType +
+			ItemSeparator +
+			*logMsg.message +
+			LineEnding,
+		),
+	)
 }
 
 func Error(desc string, err error) {
-	var (
-		endMsg string = time.Now().Format(TimeFormat) +
-			ItemSeparator +
-			ErrorItem +
-			ItemSeparator +
-			desc +
-			ERROR_WORD +
-			err.Error() +
-			LineEnding
-	)
+	*logMsg.datetime = time.Now().Format(TimeFormat)
+	*logMsg.messageType = ErrorMessageType
+	*logMsg.message = desc + ERROR_WORD + err.Error()
 
-	stderrFile.Write([]byte(endMsg))
+	stderrFile.Write(
+		[]byte(*logMsg.datetime +
+			ItemSeparator +
+			*logMsg.messageType +
+			ItemSeparator +
+			*logMsg.message +
+			LineEnding,
+		),
+	)
 }
 
 func Fatal(desc string, err error) {
-	var (
-		endMsg string = time.Now().Format(TimeFormat) +
-			ItemSeparator +
-			FatalItem +
-			ItemSeparator +
-			desc +
-			ERROR_WORD +
-			err.Error() +
-			LineEnding
-	)
+	*logMsg.datetime = time.Now().Format(TimeFormat)
+	*logMsg.messageType = FatalMessageType
+	*logMsg.message = desc + ERROR_WORD + err.Error()
 
-	stderrFile.Write([]byte(endMsg))
+	stderrFile.Write(
+		[]byte(*logMsg.datetime +
+			ItemSeparator +
+			*logMsg.messageType +
+			ItemSeparator +
+			*logMsg.message +
+			LineEnding,
+		),
+	)
 
 	os.Exit(1)
 }
